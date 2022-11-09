9009bc1fc3ceb2dd0b9bdd0a9957ebe227aff0ac
Tom Palmer <remlapmot@hotmail.com>
2022-11-09 12:27:36 +0000

    Add check for cohortextractor container

--- check-versions.sh
+++ check-versions.sh
@@ -12,3 +12,12 @@ docker run --platform linux/amd64 ghcr.io/opensafely-core/r:latest -e "R.version
 # https://github.com/opensafely-core/python-docker/pkgs/container/python
 docker pull ghcr.io/opensafely-core/python:latest
 docker run --platform linux/amd64 ghcr.io/opensafely-core/python:latest "--version"
+
+# cohortextractor
+# https://github.com/opensafely-core/cohort-extractor
+# https://github.com/opensafely-core/cohort-extractor/pkgs/container/cohortextractor
+docker pull ghcr.io/opensafely-core/cohortextractor:latest
+# Interactively
+# docker run --platform linux/amd64 --entrypoint /bin/bash -it ghcr.io/opensafely-core/cohortextractor:latest
+# interactively run:python --version
+docker run --platform linux/amd64 --entrypoint /opt/venv/bin/python ghcr.io/opensafely-core/cohortextractor:latest "--version"
